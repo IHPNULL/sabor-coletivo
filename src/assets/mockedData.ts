0d490c3c65cf57c1d763386fe74b07cf0b7707a7
feupemord <113198050+feupemord@users.noreply.github.com>
2024-04-04 23:47:53 -0300

    Update mockedData.ts

--- src/assets/mockedData.ts
+++ src/assets/mockedData.ts
@@ -42,12 +42,36 @@ export const recipe2:RecipeType = {
     ],
     steps: "Escorra o milho e use a própria lata para as medidas, Unte e enfarinhe uma forma de bolo com furo, Coloque no liquidificador o milho (já escorrido), o leite, açúcar, flocão de milho, óleo, ovos e bata bem até que o milho fique bem moído.
 Se quiser, pode acrescentar duas colheres de sopa de coco ralado, Acrescente o fermento em pó e pulse o liquidificador 3 vezes,Despeje essa massa na forma e leve ao forno médio,
-Deixe assar por, aproximadamente, 40 minutos, Faça o teste do palito e observe um tom dourado médio, para saber que o bolo está pronto, Espere esfriar totalmente para desenformar",
+Deixe assar por, aproximadamente, 40 minutos, Faça o teste do palito e observe um tom dourado médio, para saber que o bolo está pronto, Espere esfriar totalmente para desenformar
+"
 
-
+,
     calories: 140,
     portion: 60,
     protein: 6,
     carbs: 35,
     fat: 35,
+}
+
+
+export const recipe3:RecipeType = {
+    User: user1,
+    title: "Omelete rápido",
+    ingredients:[
+        "2 ovos",
+        "1 fatia de presunto",
+        "tempero verde a gosto",
+        "1 pitada de sal",
+        "2 fatias de queijo",
+        "caldo de galinha a gosto",
+    ],
+    steps: "Bata os 2 ovos, pode ser na batedeira ou não, Após ter batido bem,
+ coloque-o na frigideira já untada com óleo, acrescente o sal, 
+o presunto picado em quadradinhos e as duas fatias de queijo (não precisa picar o queijo), Coloque os temperos a gosto, espere ficar firme, 
+e vire o omelete, Está pronto um omelete delicioso, bom apetite!",
+    calories: 100,
+    portion: 35,
+    protein: 4,
+    carbs: 55,
+    fat: 10,
 }
